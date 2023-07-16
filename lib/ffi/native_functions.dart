88939dced9744cf2880be2925dcae4d5ecbd6d30
Adriano Maciel <adriano_mail@hotmail.com>
2023-07-16 17:02:55 -0300

    classe para chamar nativamente a dll windows, e tipagem correta para o play audio

---
+++ lib/ffi/native_functions.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi' as ffi;
+import 'package:ffi/ffi.dart' as ffip;
+
+typedef CPlayAudio = ffi.Void Function(ffi.Pointer<ffip.Utf8> path);
+typedef DartPlayAudio = void Function(ffi.Pointer<ffip.Utf8> path);
+
+class NativeFunctions {
+  final urnaLib = ffi.DynamicLibrary.open("native\\build\\Debug\\urna.dll");
+
+  void playSound(String path) {
+    final playAudio =
+        urnaLib.lookupFunction<CPlayAudio, DartPlayAudio>('playAudio');
+
+    playAudio(path.toNativeUtf8());
+  }
+}
